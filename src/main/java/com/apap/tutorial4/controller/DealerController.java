7109a403d5b8f0f6e7e717135ac464f32e724211
@Ronnavelly <ronnavelly@gmail.com>
2018-10-04 17:59:13 +0700

    first commit, not finished yet

---
+++ src/main/java/com/apap/tutorial4/controller/DealerController.java
@@ -0,0 +1,52 @@
+package com.apap.tutorial4.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.apap.tutorial4.model.DealerModel;
+import com.apap.tutorial4.service.CarService;
+import com.apap.tutorial4.service.DealerService;
+
+/**
+ * 
+ * DealerController
+ *
+ */
+@Controller
+public class DealerController {
+	@Autowired
+	private DealerService dealerService;
+	
+	@Autowired
+	private CarService carService;
+	
+	@RequestMapping("/")
+	private String home() {
+		return "home";
+	}
+	
+	@RequestMapping(value = "/dealer/add", method = RequestMethod.GET)
+	private String add(Model model) {
+		model.addAttribute("dealer", new DealerModel());
+		return "addDealer";
+	}
+	
+	@RequestMapping(value = "/dealer/add", method = RequestMethod.POST)
+	private String addDealerSubmit(@ModelAttribute DealerModel dealer) {
+		dealerService.addDealer(dealer);
+		return "add";
+	}
+	
+	@RequestMapping(value = "/dealer/view", method = RequestMethod.GET)
+	public String viewDealer(@RequestParam ("dealerId") long dealerId, Model model) {
+		DealerModel archive = dealerService.getDealerDetailById(dealerId).get();
+		model.addAttribute("dealer", archive);
+		return "view-dealer";
+	}
+}
---
+++ src/main/java/com/apap/tutorial4/service/CarServiceImpl.java
@@ -0,0 +1,25 @@
+package com.apap.tutorial4.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.apap.tutorial4.model.CarModel;
+import com.apap.tutorial4.repository.CarDb;
+
+/**
+ * 
+ * CarServiceImpl
+ *
+ */
+@Service
+@Transactional
+public class CarServiceImpl implements CarService{
+	@Autowired
+	private CarDb carDb;
+	
+	@Override
+	public void addCar(CarModel car) {
+		carDb.save(car);
+	}
+}
